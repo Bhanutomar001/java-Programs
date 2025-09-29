35e4135647b8b7a435649f629c5dfa7aa8ae00da
Bhanusinghtomar <amantomarru@gmail.com>
2025-09-29 21:38:14 +0530

    pattern ..

---
+++ Printfactorial.java
@@ -0,0 +1,12 @@
+public class Printfactorial {
+    public static void main(String[] args){
+        long mul=1;
+        int n=20;
+        for(int i=1; i<=n; i++){
+          mul=mul*i;
+         // System.out.println(mul);
+        }
+         System.out.println(mul);
+    }
+}
+o8
