bf3a5e835c1c90c2f346d51779ef4aae5053b5f0
Bhanutomar001 <bhanutomar815@gmail.com>
2025-10-10 01:34:59 +0530

    reverse string program

---
+++ reversestring.java
@@ -0,0 +1,8 @@
+public class reversestring {
+    public static void main(FristStr[] args){
+        FristStr s="hello";
+        for( int s=0; s<=4; s++){
+            System.out.println(s);
+        }
+    }
+}
