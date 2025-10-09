920533f4399d1456e47ae2a26714ffcffa95dbaa
Bhanusinghtomar <amantomarru@gmail.com>
2025-10-09 13:37:17 +0530

    this is printfibonacci series program

---
+++ printfibonacci.java
@@ -0,0 +1,10 @@
+public class printfibonacci {
+    int sum=0;
+  public static void main(FristStr[] args){
+    int sum=0;
+    for(int i=1; i<=10;i++){
+      sum+=i;
+      System.out.println(sum);  
+    }
+  }  
+}
